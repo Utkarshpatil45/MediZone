ad6d23b388f0b0a4cb4bda90b497c7e843c4040e
simple chatbot <utkarshpatil2525.in>
2026-06-08 00:27:22 +0530

    Database config updated

--- app.py
+++ app.py
@@ -13,10 +13,15 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "medizone_secret_123"
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Patil'
-app.config['MYSQL_DB'] = 'medizone'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'Patil'
+# app.config['MYSQL_DB'] = 'medizone'
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
